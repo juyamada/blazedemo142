11e6ddc390e23e3316bb64658ade01cd6d5aed70
juyamada <jysyamada@gmail.com>
2024-11-05 22:38:12 -0300

    criamos o arquivo compra passagem BDD.java

---
+++ src/test/java/steps/ComprarPassagemBDD.java
@@ -0,0 +1,25 @@
+package steps;
+
+import io.cucumber.java.pt.Dado;
+import io.cucumber.java.pt.E;
+import io.cucumber.java.pt.Entao;
+import io.cucumber.java.pt.Quando;
+
+public class ComprarPassagemBDD {
+@Dado("que acesso o site {string}")
+public void que_acesso_o_site(String string) {
+
+}
+@Quando("seleciono a origem {string} e destino {string}")
+public void seleciono_a_origem_e_destino(String string, String string2) {
+    
+}
+@E("clico no botao Find Flights")
+public void clico_no_botao_find_flights() {
+
+}
+@Entao("visualizo a lista de voos")
+public void visualizo_a_lista_de_voos() {
+    
+}  
+}
